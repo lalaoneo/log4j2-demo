71d5d2288638b3e6e541274d3b228ec1103ef2d3
lori <40824190+lalaoneo@users.noreply.github.com>
2019-01-16 11:34:26 +0800

    Create Log4j2Test.java

---
+++ src/test/java/com/lori/log4j2demo/log/Log4j2Test.java
@@ -0,0 +1,21 @@
+package com.lori.log4j2demo.log;
+
+import com.lori.log4j2demo.utils.Log;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Log4j2Test {
+
+    private static Logger logger = LoggerFactory.getLogger(Log.class);
+
+    @Test
+    public void testLog4j2(){
+        logger.info("测试日志能正常打印");
+    }
+}
